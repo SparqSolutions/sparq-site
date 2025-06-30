eb338e61cc1391145554c9572e472e74338899d3
Julien Curnutt <julien@sparqsolutionsai.com>
2025-06-30 12:29:08 -0500

    Updates

---
+++ _app/immutable/chunks/-4x8eEsB.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_rww3na)==null?void 0:s.base)??"/sparq-site";var a;const t=((a=globalThis.__sveltekit_rww3na)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/chunks/DTq-CJgG.js
@@ -0,0 +1,1 @@
+import{Z as a}from"./B2bvgy9_.js";a();
