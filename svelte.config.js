82b4eeaecb133d7080f78966a7b7161c105d3f11
Julien Curnutt <julien@sparqsolutionsai.com>
2025-06-30 12:22:34 -0500

    feat: configure for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter(),
+		paths: {
+			base: process.argv.includes('dev') ? '' : '/sparq-site'
+		}
+	}
 };
 
 export default config;
